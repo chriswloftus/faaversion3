f96aa62bd93370db341a6a6e026fa6c0e3e5ccbb
Chris Loftus <cwl@aber.ac.uk>
2020-10-20 11:55:07 +0100

    Base line of the project before workshop 9 additions

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsFragment.kt
@@ -13,6 +13,6 @@ class CatsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_cats, container, false)
     }
 }
